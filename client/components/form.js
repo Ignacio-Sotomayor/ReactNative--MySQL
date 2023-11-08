57b421fe09d371fee1e66dc510f274f0a5a623cb
Sotomayor Ignacio <sotomayornachi05@gmail.com>
2023-11-08 17:18:47 -0300

    final commit

--- client/components/form.js
+++ client/components/form.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { StyleSheet,TextInput, Button, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, TextInput, Button, Text, View, ScrollView } from 'react-native';
 import { useState, useEffect } from 'react';
 import { useForm, Controller } from "react-hook-form";
 import axios from 'axios';
 
-function form(){
+function form() {
   const { control, handleSubmit, errors } = useForm();
   return (
     <View>
@@ -12,7 +12,7 @@ function form(){
         control={control}
         render={({ onChange, onBlur, value }) => (
           <TextInput
-            style={{paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8}}
+            style={{ paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8 }}
             onBlur={onBlur}
             onChangeText={value => onChange(value)}
             value={value}
@@ -27,14 +27,14 @@ function form(){
         control={control}
         render={({ onChange, onBlur, value }) => (
           <TextInput
-            style={{paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8}}
+            style={{ paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8 }}
             onBlur={onBlur}
             onChangeText={value => onChange(value)}
             value={value}
           />
         )}
         name="lastName"
-        rules={{ required: true, minLength: 8}}
+        rules={{ required: true, minLength: 8 }}
         defaultValue=""
       />
       {errors.lastName?.type === "required" && <Text>Last Name is required.</Text>}
@@ -47,31 +47,31 @@ z3
 
 export default function Lista() {
 
-    const [alumnos, setAlumnos] = useState([]);
-    
-    const data = [
-    ]
+  const [alumnos, setAlumnos] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://10.0.0.248:8080/')
-            .then(response => setAlumnos(response.data))
-            .catch(error => console.error(error));
-    }, [])
+  const data = [
+  ]
+
+  useEffect(() => {
+    axios.get('http://10.0.0.248:8080/')
+      .then(response => setAlumnos(response.data))
+      .catch(error => console.error(error));
+  }, [])
 
 
-    return (
-        <View style={styles.container}>
-            
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, paddingTop: 400, backgroundColor: '#fff' },
-  head: {  height: 40,  backgroundColor: '#f1f8ff'  },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
   wrapper: { flexDirection: 'row' },
   title: { flex: 1, backgroundColor: '#f6f8fa' },
-  row: {  height: 56, width:750},
-  rows: {  height: 56, width:750},
+  row: { height: 56, width: 750 },
+  rows: { height: 56, width: 750 },
   text: { textAlign: 'center' }
 });
